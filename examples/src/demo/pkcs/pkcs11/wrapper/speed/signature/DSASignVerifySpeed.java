76efecefb18cac1113d1aa3a807bb293846f38e1
Lijun Liao <lijun.liao@gmail.com>
2019-07-01 23:27:01 +0200

    add speed test for DSA sign / verify

---
+++ examples/src/demo/pkcs/pkcs11/wrapper/speed/signature/DSASignVerifySpeed.java
@@ -0,0 +1,112 @@
+package demo.pkcs.pkcs11.wrapper.speed.signature;
+
+import org.junit.Test;
+import org.xipki.util.BenchmarkExecutor;
+
+import demo.pkcs.pkcs11.wrapper.TestBase;
+import demo.pkcs.pkcs11.wrapper.util.Util;
+import iaik.pkcs.pkcs11.Mechanism;
+import iaik.pkcs.pkcs11.Token;
+import iaik.pkcs.pkcs11.TokenException;
+import iaik.pkcs.pkcs11.objects.DSAPrivateKey;
+import iaik.pkcs.pkcs11.objects.DSAPublicKey;
+import iaik.pkcs.pkcs11.objects.PrivateKey;
+import iaik.pkcs.pkcs11.objects.PublicKey;
+import iaik.pkcs.pkcs11.wrapper.Functions;
+import iaik.pkcs.pkcs11.wrapper.PKCS11Constants;
+import junit.framework.Assert;
+
+public class DSASignVerifySpeed extends TestBase {
+
+  private class MySignExecutor extends SignExecutor {
+
+    public MySignExecutor(Token token, char[] pin) throws TokenException {
+      super(Functions.mechanismCodeToString(signMechanism)
+          + " (P:2048, Q:256) Sign Speed",
+          Mechanism.get(keypairGenMechanism), token, pin,
+          Mechanism.get(signMechanism), 32);
+    }
+
+    @Override
+    protected PrivateKey getMinimalPrivateKeyTemplate() {
+      return getMinimalPrivateKeyTemplate0();
+    }
+
+    @Override
+    protected PublicKey getMinimalPublicKeyTemplate() {
+      return getMinimalPublicKeyTemplate0();
+    }
+
+  }
+
+  private class MyVerifyExecutor extends VerifyExecutor {
+
+    public MyVerifyExecutor(Token token, char[] pin) throws TokenException {
+      super(Functions.mechanismCodeToString(signMechanism)
+          + " (P:2048, Q:256) Sign Speed",
+          Mechanism.get(keypairGenMechanism), token, pin,
+          Mechanism.get(signMechanism), 32);
+    }
+
+    @Override
+    protected PrivateKey getMinimalPrivateKeyTemplate() {
+      return getMinimalPrivateKeyTemplate0();
+    }
+
+    @Override
+    protected PublicKey getMinimalPublicKeyTemplate() {
+      return getMinimalPublicKeyTemplate0();
+    }
+
+  }
+
+  private static final long keypairGenMechanism =
+      PKCS11Constants.CKM_DSA_KEY_PAIR_GEN;
+
+  private static final long signMechanism = PKCS11Constants.CKM_DSA;
+
+  private PrivateKey getMinimalPrivateKeyTemplate0() {
+    return new DSAPrivateKey();
+  }
+
+  private PublicKey getMinimalPublicKeyTemplate0() {
+    DSAPublicKey publicKeyTemplate = new DSAPublicKey();
+    publicKeyTemplate.getPrime().setByteArrayValue(
+        Functions.decodeHex(DSA_P));
+    publicKeyTemplate.getSubprime().setByteArrayValue(
+        Functions.decodeHex(DSA_Q));
+    publicKeyTemplate.getBase().setByteArrayValue(
+        Functions.decodeHex(DSA_G));
+    publicKeyTemplate.getToken().setBooleanValue(Boolean.FALSE);
+    return publicKeyTemplate;
+  }
+
+  @Test
+  public void main() throws TokenException {
+    Token token = getNonNullToken();
+    if (!Util.supports(token, keypairGenMechanism)) {
+      System.out.println(Functions.mechanismCodeToString(keypairGenMechanism)
+          + " is not supported, skip test");
+      return;
+    }
+
+    if (!Util.supports(token, signMechanism)) {
+      System.out.println(Functions.mechanismCodeToString(signMechanism)
+          + " is not supported, skip test");
+      return;
+    }
+
+    BenchmarkExecutor executor = new MySignExecutor(token, getModulePin());
+    executor.setThreads(getSpeedTestThreads());
+    executor.setDuration(getSpeedTestDuration());
+    executor.execute();
+    Assert.assertEquals("Sign speed", 0, executor.getErrorAccout());
+
+    executor = new MyVerifyExecutor(token, getModulePin());
+    executor.setThreads(getSpeedTestThreads());
+    executor.setDuration(getSpeedTestDuration());
+    executor.execute();
+    Assert.assertEquals("Verify speed", 0, executor.getErrorAccout());
+  }
+
+}
